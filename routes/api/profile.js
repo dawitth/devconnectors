1ce93ec602b081855098208f541f533a5971f0ae
Dawit <dawithunegnaw@uvic.ca>
2019-04-27 14:31:19 -0700

    updated profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+// Load Profile Model
+const Profile = require('../../models/Profile')
+
+// Load User Model
+const User = require('../../models/User')
 
 
 // @routes GET api/profile/test
@@ -10,6 +17,31 @@ const router = express.Router();
 router.get('/test', (req,res)=>{
 	res.json({msg: "Profile works"})
 })
+
+
+// @routes GET api/profile/
+// @description Tests profile route
+// @access Private
+
+router.get('/', passport.authenticate('jwt',{session:false}), (req,res) =>{
+	const errors = {}
+ 	Profile.findOne({ 
+ 		user: req.user.id
+ 	})
+ 	.then(profile=>{
+ 		if(!profile){
+ 			errors.noprofile = 'There is no profile for this user'
+ 			return res.status(404).json(errors);
+ 		} 
+ 			res.json(profile)
+ 		
+ 	})
+ 	.catch(err => res.status(404).json(err));
+ 	
+})
+
+
+
 
 
 module.exports = router;
