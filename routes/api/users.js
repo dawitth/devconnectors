f9c69136144e0a798cab5d59e588b03db86611af
Dawit <dawithunegnaw@uvic.ca>
2019-04-26 17:43:50 -0700

    added user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+
+// Load user model
+const User = require('../../models/User')
 
 
 
@@ -9,6 +15,44 @@ const router = express.Router();
 
 router.get('/test', (req,res)=>{
 	res.json({msg: "Users works"})
+})
+
+// @routes GET api/users/register
+// @description to register users
+// @access Public
+
+
+router.post('/register', (req, res) => {
+	// check if the email doesnt exisit
+	User.findOne({ email: req.body.email})
+		.then(user => {
+			if(user){
+				return res.status(400).json({email:"email already exists"})
+			} else {
+				const avatar = gravatar.url(req.body.email,{
+					s: '200', // size
+					r: 'pg', // Rating
+					d: 'mm' // default
+				})
+				const newUser = new User({
+					name: req.body.name,
+					email: req.body.email,
+					avatar,
+					password: req.body.password
+
+				});
+				bcrypt.genSalt(10, (err,salt) => {
+					bcrypt.hash(newUser.password, salt, (err, hash) =>{
+						if(err) throw err;
+						newUser.password = hash;
+						newUser.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err))
+					})
+				})
+			}
+		})
+
 })
 
 
