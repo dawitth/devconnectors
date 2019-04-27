784794c9ce9ecde7f3d54424c1669718c6c3b027
Dawit <dawithunegnaw@uvic.ca>
2019-04-27 12:50:20 -0700

    added validation for email, name, password

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,11 @@ const keys = require('../../config/keys')
 const passport = require('passport')
 
 
+// Load Input Validation
+
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
 // Load user model
 const User = require('../../models/User')
 
@@ -26,11 +31,20 @@ router.get('/test', (req,res)=>{
 
 
 router.post('/register', (req, res) => {
+
+	const {errors, isValid} = validateRegisterInput(req.body);
+
+	// Check Validation
+	if(!isValid){
+		return res.status(400).json(errors)
+	}
+
 	// check if the email doesnt exisit
 	User.findOne({ email: req.body.email})
 		.then(user => {
 			if(user){
-				return res.status(400).json({email:"email already exists"})
+				errors.email = "Email already exists"
+				return res.status(400).json(errors)
 			} else {
 				const avatar = gravatar.url(req.body.email,{
 					s: '200', // size
@@ -63,6 +77,16 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req,res) => {
+
+
+	const {errors, isValid} = validateLoginInput(req.body);
+
+	// Check Validation
+	if(!isValid){
+		return res.status(400).json(errors)
+	}
+
+
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -72,7 +96,8 @@ router.post('/login', (req,res) => {
 		.then( user=>{
 			// check for user
 			if(!user){
-				return res.status(404).json({email: "user email not found"})
+				errors.email = "User email not found"
+				return res.status(404).json(errors)
 			}
 
 			// check password
@@ -108,7 +133,8 @@ router.post('/login', (req,res) => {
 
 				  		
 				  	} else {
-				  		return res.status(400).json({password: 'password incorrect'})
+				  		errors.password = "password incorrect"
+				  		return res.status(400).json(errors)
 				  	}
 				  })
 		})
